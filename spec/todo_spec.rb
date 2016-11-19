b4605600d713ca2924101d8010051c979a7e3a5b
AndyDeany <oneandydean@hotmail.com>
2016-11-19 19:15:46 +0000

    Create unit tests for Todo.sync functionality

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -1,5 +1,5 @@
 describe Todo do
-  before(:each) { Todo.class_variable_set :@@todos, [] }
+  before(:each) { Todo.class_variable_set(:@@todos, []) }
   # Initialisation
   it 'should initialise with both arguments' do
     todo = Todo.new('Remember the milk', Date.parse('21 Feb 2015'))
@@ -37,6 +37,9 @@ describe Todo do
     todo.due_date = Date.parse('21 Feb 2015')
     expect(todo.due_date).to eq Date.parse('21 Feb 2015')
   end
+
+  #it 'should only allow us to assign Date objects to due_date' do
+  #end
 
   # Behaviour
   it 'should allow us to get an array of all instantiated todos' do
@@ -84,10 +87,67 @@ describe Todo do
     todo5 = Todo.new('Go to sleep')
     todo6 = Todo.new('Remember the milk')
     Todo.sync
-    Todo.class_variable_set :@@todos, []
+    Todo.class_variable_set(:@@todos, [])
     Todo.sync
-    expect(Todo.all).to eq [todo1, todo2, todo3, todo4, todo5, todo6]
-    # Objects received won't be the same. Needs updating
-    # might need changing to "(Todo.all.include? [todos]).to eq true
+
+    sent_data = []
+    [todo1, todo2, todo3, todo4, todo5, todo6].each do |todo|
+      sent_data.push [
+        todo.instance_variable_get('@id'),
+        todo.title,
+        todo.due_date
+      ]
+    end
+    received_data = []
+    puts Todo.all.length
+    Todo.all.each do |todo|
+      received_data.push [
+        todo.instance_variable_get('@id'),
+        todo.title,
+        todo.due_date
+      ]
+    end
+
+    expect(sent_data - received_data).to eq []
+  end
+
+  it 'should assign each todo a "created_at" date/time when synced' do
+    Todo.new('Remember the milk', '2016-12-21')
+    Todo.sync
+    expect(HTTParty.get(
+      "http://lacedeamon.spartaglobal.com/todos/#{todo.id}"
+    )['created_at'][0..-6]).to eq DateTime.now.strftime('%Y-%M-%dT%H:%M:%S')
+    # (should be correct to the nearest second, but get requests take time...)
+    # [insert get request for date here. Parse the date and time to check it's
+    #  correct, or at least pretty close]
+  end
+
+  it 'should assign each todo an "updated_at" date/time when synced' do
+    todo = Todo.new('Remember the milk', '2016-12-21')
+    Todo.sync
+    get_request1 = HTTParty.get(
+      "http://lacedeamon.spartaglobal.com/todos/#{todo.id}"
+    )
+    expect(get_request1['updated_at']).to eq get_request1['created_at']
+    todo.title = 'Get some bread instead'
+    Todo.sync
+    get_request2 = HTTParty.get(
+      "http://lacedeamon.spartaglobal.com/todos/#{todo.id}"
+    )
+    expect(get_request2['updated_at'][0..-6]).to eq(
+      DateTime.now.strftime('%Y-%M-%dT%H:%M:%S')
+    )
+    expect(get_request2['updated_at']).not_to eq get_request2['created_at']
+    expect(get_request2['created_at']).not_to eq get_request1['created_at']
+  end
+
+  it 'should update the same todo in the server if a todo is updated' do
+    todo = Todo.new('Remember the milk', '2016-12-21')
+    Todo.sync
+    todo.title = 'Get some bread instead'
+    Todo.sync
+    expect(HTTParty.get(
+      "http://lacedeamon.spartaglobal.com/todos/#{todo.id}"
+    )['title']).to eq 'Get some bread instead'
   end
 end
